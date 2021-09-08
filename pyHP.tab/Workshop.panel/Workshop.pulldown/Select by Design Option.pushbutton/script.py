c52aae7eb37a0db47262fa3a8b707cfaf73d3e6e
rastermadre <daria.ivanciucova@gmail.com>
2021-09-08 12:47:55 +0100

    Added Select by Design Option button

---
+++ pyHP.tab/Workshop.panel/Workshop.pulldown/Select by Design Option.pushbutton/script.py
@@ -0,0 +1,39 @@
+__title__ = "Select by Design Option"
+__doc__ = "Select elements of a design option"
+
+
+from pyrevit import revit, DB, UI
+from rpw.ui.forms import (FlexForm, Label, ComboBox, Separator, Button)
+import sys
+
+
+design_options = DB.FilteredElementCollector(revit.doc).OfClass(DB.DesignOption).ToElements()
+
+do_dict = {do.Name: do for do in design_options}
+
+# construct rwp UI
+components = [
+    Label("Select Design Option:"),
+    ComboBox(name="design_option", options = do_dict),
+    Button("Select")]
+form = FlexForm("Select Elements by Design Option", components)
+form.show()
+# assign chosen parameters
+chosen_do = form.values["design_option"]
+
+if not chosen_do:
+    sys.exit()
+
+
+selection = revit.get_selection()
+
+selSet = []
+
+do_filter = DB.ElementDesignOptionFilter(chosen_do.Id)
+do_el_list = DB.FilteredElementCollector(revit.doc).WherePasses(do_filter).ToElementIds()
+
+for elid in do_el_list:
+    selSet.append(elid)
+
+selection.set_to(selSet)
+revit.uidoc.RefreshActiveView()
