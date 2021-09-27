5a9f4da675ac29c0a21575ecfa59c52879afdd21
rastermadre <daria.ivanciucova@gmail.com>
2021-09-27 13:56:19 +0100

    added Legend tool to Workshop

--- pyHP.tab/Tiny Tools.panel/Cleanup.pulldown/Remove Unused Scope Boxes.pushbutton/script.py
+++ pyHP.tab/Tiny Tools.panel/Cleanup.pulldown/Remove Unused Scope Boxes.pushbutton/script.py
@@ -1,26 +1,23 @@
 __title__ = "Purge Unused Scope Boxes"
 
-
 from pyrevit import revit, DB, forms, script
-import xlrd
-from rpw.ui.forms import (FlexForm, Label, ComboBox, Separator, Button)
-import sys
 
 
 def filter_sb_viewtypes(views):
-    """returns only views of types compatible with scope box """
+    # returns only views of types compatible with scope box
     compatible_viewtypes = [
-            DB.ViewType.FloorPlan,
-            DB.ViewType.CeilingPlan,
-            DB.ViewType.EngineeringPlan,
-            DB.ViewType.AreaPlan,
-            DB.ViewType.Section,
-            DB.ViewType.Elevation,
-            DB.ViewType.Detail,
-            DB.ViewType.ThreeD
+        DB.ViewType.FloorPlan,
+        DB.ViewType.CeilingPlan,
+        DB.ViewType.EngineeringPlan,
+        DB.ViewType.AreaPlan,
+        DB.ViewType.Section,
+        DB.ViewType.Elevation,
+        DB.ViewType.Detail,
+        DB.ViewType.ThreeD
     ]
     filtered_views = [v for v in views if v.ViewType in compatible_viewtypes]
     return filtered_views
+
 
 # collect all scope boxes
 coll_scope = DB.FilteredElementCollector(revit.doc).OfCategory(DB.BuiltInCategory.OST_VolumeOfInterest).ToElementIds()
@@ -39,7 +36,7 @@ for view in compatible_views:
     except:
         pass
 
-with revit.Transaction("Remove Unused Scope Boxes"):
+with revit.Transaction("Purge Unused Scope Boxes"):
     # get ids of areas with location and zero area
     unused_ids = [sb for sb in coll_scope if sb not in set(used_sb)]
 
@@ -47,13 +44,13 @@ with revit.Transaction("Remove Unused Scope Boxes"):
 
     # remove unused scope boxes
     for usb_id in unused_ids:
-
         deleted.append(revit.doc.GetElement(usb_id).Name)
         revit.doc.Delete(usb_id)
 
-
     # print result
-    print("Removed unused scope boxes:")
-
-    for d in deleted:
-        print (d)
+    if not deleted:
+        print ("No Scope Boxes were deleted.")
+    else:
+        print("Purged unused Scope Boxes:")
+        for d in deleted:
+            print(d)
