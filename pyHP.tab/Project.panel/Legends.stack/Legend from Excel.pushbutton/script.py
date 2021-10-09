e2e41512b473b1772c08fd9b3220587acb0a695d
rastermadre <daria.ivanciucova@gmail.com>
2021-10-10 00:23:11 +0100

    Update script.py

--- pyHP.tab/Project.panel/Legends.stack/Legend from Excel.pushbutton/script.py
+++ pyHP.tab/Project.panel/Legends.stack/Legend from Excel.pushbutton/script.py
@@ -2,8 +2,15 @@
 from pyrevit.framework import List
 from pyrevit import revit, DB, forms, script
 import xlrd
-from rpw.ui.forms import (FlexForm, Label, ComboBox, Separator, Button)
+from rpw.ui.forms import (FlexForm, Label, ComboBox, Separator, Button, TextBox)
 from collections import OrderedDict
+
+
+def convert_length_to_internal(from_units):
+    # convert length units from project  to internal
+    d_units = DB.Document.GetUnits(revit.doc).GetFormatOptions(DB.UnitType.UT_Length).DisplayUnits
+    converted = DB.UnitUtils.ConvertToInternalUnits(from_units, d_units)
+    return converted
 
 
 def any_fill_type():
@@ -100,22 +107,37 @@ for i in range(0, worksheet.nrows):
 txt_types = DB.FilteredElementCollector(revit.doc).OfClass(DB.TextNoteType)
 text_style_dict= {txt_t.get_Parameter(DB.BuiltInParameter.SYMBOL_NAME_PARAM).AsString(): txt_t for txt_t in txt_types}
 
+create_new_fill_types = True
+#create_new_fill_types = False
+
 # construct rwp UI
 components = [
     Label("Pick Text Style:"),
     ComboBox(name="textstyle_combobox1", options=text_style_dict),
+    Label("Box Width [mm]"),
+    TextBox(name="box_width", Text="1000"),
+    Label("Box Height [mm]"),
+    TextBox(name="box_height", Text="240"),
+    Label("Offset [mm]"),
+    TextBox(name="box_offset", Text="80"),
+    Label("Create new Filled Region Types"),
+    ComboBox(name="create_new", options={"Create New": True, "Use Overrides" : False}),
     Button("Select")]
 form = FlexForm("Appearance", components)
 form.show()
 # assign chosen parameters
 chosen_text_style = form.values["textstyle_combobox1"]
+box_width = int(form.values["box_width"])
+box_height = int(form.values["box_height"])
+box_offset = int(form.values["box_offset"])
+create_new_fill_types = form.values["create_new"]
 
 # dims and scale
 scale = float(view.Scale)/100
-w = 3.28 * scale
-h = 0.787 * scale
+w = convert_length_to_internal(box_width) * scale
+h = convert_length_to_internal(box_height) * scale
 text_offset = 1 * scale
-shift = 1.076 * scale
+shift = (convert_length_to_internal(box_offset)+w) * scale
 h_offset = 5.25 * scale
 # create rectrangle
 crv_loop = DB.CurveLoop()
@@ -136,22 +158,6 @@ orig_rectangle = [l1, l2, l3, l4]
 offset = 0
 origin = DB.XYZ(0,shift,0)
 
-# with revit.Transaction("Create Filled Reg"):
-#     fr_types = DB.FilteredElementCollector(revit.doc).OfClass(DB.FilledRegionType)
-#
-#     for tx_value in colour_scheme_od.keys():
-#         found = False
-#         for fr_type in fr_types:
-#             if fr_type.get_Parameter(DB.BuiltInParameter.SYMBOL_NAME_PARAM).AsString() == tx_value:
-#                 colour_scheme_od[tx_value] = fr_type
-#                 found = True
-#         if not found:
-#             new_type = any_fill_type().Duplicate(tx_value)
-#             new_type.ForegroundPatternColor = colour_scheme_od[header][tx_value]
-#             new_type.ForegroundPatternId  = get_solid_fill_pat().Id
-#             colour_scheme_od[tx_value] = new_type
-
-
 with revit.Transaction("Draw Legend"):
     fr_types = DB.FilteredElementCollector(revit.doc).OfClass(DB.FilledRegionType)
     for header in colour_scheme_od:
@@ -166,26 +172,29 @@ with revit.Transaction("Draw Legend"):
         origin = DB.XYZ(origin.X, -(offset), origin.Z)
         for fill_name in colour_scheme_od[header]:
 
+            # if chosen to create new fill types
+            if create_new_fill_types:
+                found = False
+                for fr_type in fr_types:
+                    if fr_type.get_Parameter(DB.BuiltInParameter.SYMBOL_NAME_PARAM).AsString() == fill_name:
+                        colour_scheme_od[header][fill_name] = fr_type
+                        found = True
+                if not found:
+                    new_type = any_fill_type().Duplicate(fill_name)
+                    new_type.ForegroundPatternColor = colour_scheme_od[header][fill_name]
+                    new_type.ForegroundPatternId = get_solid_fill_pat().Id
+                    colour_scheme_od[header][fill_name] = new_type
 
-            found = False
-            for fr_type in fr_types:
-                if fr_type.get_Parameter(DB.BuiltInParameter.SYMBOL_NAME_PARAM).AsString() == fill_name:
-                    colour_scheme_od[header][fill_name] = fr_type
-                    found = True
-            if not found:
-                new_type = any_fill_type().Duplicate(fill_name)
-                new_type.ForegroundPatternColor = colour_scheme_od[header][fill_name]
-                new_type.ForegroundPatternId = get_solid_fill_pat().Id
-                colour_scheme_od[header][fill_name] = new_type
 
-
-            # draw rectangles with filled region
-            new_reg = draw_rectangle(offset, colour_scheme_od[header][fill_name], view, invis_style())
-            # # override fill and colour
-            # ogs = DB.OverrideGraphicSettings()
-            # ogs.SetSurfaceForegroundPatternColor(colour_scheme_od[header][component])
-            # ogs.SetSurfaceForegroundPatternId(get_solid_fill_pat().Id)
-            # view.SetElementOverrides(new_reg.Id, ogs)
+                # draw rectangles with filled region
+                new_reg = draw_rectangle(offset, colour_scheme_od[header][fill_name], view, invis_style())
+            else:
+                new_reg = draw_rectangle(offset, any_fill_type(), view, invis_style())
+                # override fill and colour
+                ogs = DB.OverrideGraphicSettings()
+                ogs.SetSurfaceForegroundPatternColor(colour_scheme_od[header][fill_name])
+                ogs.SetSurfaceForegroundPatternId(get_solid_fill_pat().Id)
+                view.SetElementOverrides(new_reg.Id, ogs)
 
             # place text next to filled regions
             label_position = DB.XYZ(w+text_offset, -(offset-h), 0)
