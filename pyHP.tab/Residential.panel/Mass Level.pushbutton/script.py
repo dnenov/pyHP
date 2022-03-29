8678aaef5462b1fc00bcc77eed5fdb828589d442
rastermadre <daria.ivanciucova@gmail.com>
2022-03-29 17:30:04 +0100

    added mass level

---
+++ pyHP.tab/Residential.panel/Mass Level.pushbutton/script.py
@@ -0,0 +1,31 @@
+import sys
+
+from pyrevit import revit, DB, forms
+from rpw.ui.forms import FlexForm, Label, TextBox, Button, ComboBox, CheckBox, Separator
+
+
+masses = DB.FilteredElementCollector(revit.doc).OfCategory(DB.BuiltInCategory.OST_Mass).WhereElementIsNotElementType().ToElements()
+
+params = [p.Definition.Name for p in masses[0].Parameters if p.StorageType.ToString() == "String"]
+
+components = [
+    Label("Select Mass parameter"),
+    ComboBox(name="p", options=params),
+    Button("Select")
+    ]
+
+form = FlexForm("Select", components)
+ok = form.show()
+
+if ok:
+    # match the variables with user input
+    chosen_param = form.values["p"]
+else:
+    sys.exit()
+# selected_param = forms.select_parameters(masses[0], title="Select Parameter for Level", multiple = False, include_instance=True, include_type=False)
+# print (selected_param.definition)
+with revit.Transaction("Fill in Mass Levels",revit.doc):
+    for m in masses:
+        workplane = m.Host.get_Parameter(DB.BuiltInParameter.DATUM_TEXT).AsString()
+        m.LookupParameter(chosen_param).Set(workplane)
+        # print (workplane)
