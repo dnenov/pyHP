04a5ea6e8d2aec1af6d4f29e8306543c1d8f5386
dariaivanciucova <divanciucova@archilizer.com>
2024-02-20 18:12:53 +0000

    fix bugs, add decimals to Unit Mix

--- pyHP.tab/Residential.panel/Units Mix.pushbutton/script.py
+++ pyHP.tab/Residential.panel/Units Mix.pushbutton/script.py
@@ -4,46 +4,71 @@ import tempfile
 import re
 import sys
 
+FEC = DB.FilteredElementCollector
+BIC = DB.BuiltInCategory
 doc = revit.doc
-collect_units = DB.FilteredElementCollector(doc).OfCategory(
-    DB.BuiltInCategory.OST_GenericModel).WhereElementIsNotElementType().ToElements()
+active_view = revit.active_view
+output = script.get_output()
+
+
+# get the design option of the active view
+def get_design_option_of_view(view):
+    units_in_view = FEC(doc, active_view.Id).OfCategory(BIC.OST_GenericModel).WhereElementIsNotElementType().ToElements()
+    for unit in units_in_view:
+        try:
+            tenure = doc.GetElement(unit.GetTypeId()).LookupParameter("Tenure").AsValueString()
+            if tenure:
+                return unit
+        except:
+            return None
+
+one_visible_unit = get_design_option_of_view(active_view)
+design_option = one_visible_unit.DesignOption
+
+collect_all_units = FEC(doc).OfCategory(BIC.OST_GenericModel).WhereElementIsNotElementType().ToElements()
+
+units_of_active_design_option = [unit for unit in collect_all_units if unit.DesignOption.Id == design_option.Id]
+
+if len (units_of_active_design_option) == 0:
+    output.print_md("Zero units found")
+    sys.exit()
 
 # counters
 private_hab_rooms = 0
 shared_hab_rooms = 0
 social_hab_rooms = 0
 
-for unit in collect_units:
+for unit in units_of_active_design_option:
     unit_type = doc.GetElement(unit.GetTypeId())
-    tenure = unit_type.LookupParameter("Tenure").AsValueString()
-    print (tenure)
-    hab_rooms = unit_type.LookupParameter("Habitable Rooms").AsValueString()
+    try:
+        tenure = unit_type.LookupParameter("Tenure").AsValueString()
+        hab_rooms = unit_type.LookupParameter("Habitable Rooms").AsValueString()
 
-    if tenure == "Private":
-        private_hab_rooms += int(hab_rooms)
-    elif tenure == "S/O" or tenure == "Shared Ownership":
-        shared_hab_rooms += int(hab_rooms)
-    if tenure == "Rented" or tenure == "Social Rent":
-        social_hab_rooms += int(hab_rooms)
-
+        if tenure == "Private":
+            private_hab_rooms += int(hab_rooms)
+        elif tenure == "S/O" or tenure == "Shared Ownership":
+            shared_hab_rooms += int(hab_rooms)
+        if tenure == "Rented" or tenure == "Social Rent":
+            social_hab_rooms += int(hab_rooms)
+    except:
+        pass
 
 # calculate
 affordable_room_count = social_hab_rooms + shared_hab_rooms
 total_room_count = private_hab_rooms + affordable_room_count
-private_percent = private_hab_rooms* 100/total_room_count
-affordable_percent = affordable_room_count* 100/total_room_count
-shared_percent = shared_hab_rooms* 100/affordable_room_count
-social_percent = social_hab_rooms* 100/affordable_room_count
 
+
+private_percent = float(private_hab_rooms* 100.0/total_room_count)
+affordable_percent = float(affordable_room_count* 100.0/total_room_count)
+shared_percent = float(shared_hab_rooms* 100.0/affordable_room_count)
+social_percent = float(social_hab_rooms* 100.0/affordable_room_count)
 
 private_colour = "#7E95B3"
 affordable_colour = "#B3B133"
 shared_colour = "#559050"
 social_colour = "#FFA037"
 
-output = script.get_output()
-
-output.print_md("# Units Mix")
+output.print_md("# Units Mix - Option {}".format(design_option.Name))
 output.print_md("--------")
 
 # Private / Affordable split
@@ -66,9 +91,9 @@ chartPrivateAffordable.draw()
 
 
 output.print_md(" . . . . . Habitable Rooms | Percent")
-output.print_md("Private . . . . . . . . {0} | {1} %".format(private_hab_rooms, private_percent))
-output.print_md("Affordable . . . . . . {0} | {1} %".format(affordable_room_count, affordable_percent))
-
+output.print_md("Private . . . . . . . . {} | {:.2f} %".format(private_hab_rooms, private_percent))
+output.print_md("Affordable . . . . . . {} | {:.2f} %".format(affordable_room_count, affordable_percent))
+output.print_md("TOTALS . . . . . . . . {}".format(total_room_count))
 output.print_md("--------")
 
 # Affordable split chart
@@ -90,5 +115,7 @@ chartAffordableSplit.set_height = 1
 chartAffordableSplit.draw()
 
 output.print_md(". . . . . . . . . .Habitable Rooms     |         Percent")
-output.print_md("Shared Ownership. . . . . . . . {0}            |            {1} %".format(shared_hab_rooms, shared_percent))
-output.print_md("Social Rent . . . . . . . . . . {0}            |            {1} %".format(str(social_hab_rooms), social_percent))
+output.print_md("Shared Ownership. . . . . . . . {}            |            {:.2f} %".format(shared_hab_rooms, shared_percent))
+output.print_md("Social Rent . . . . . . . . . . {}            |            {:.2f} %".format(str(social_hab_rooms), social_percent))
+output.print_md("TOTALS . . . . . . . . {}".format(affordable_room_count))
+output.print_md("--------")
